f4f5a63ffb19704a1ab1cadcc7aa848d7b68f7bf
archmoj <mojtaba@plot.ly>
2018-10-02 14:12:50 -0400

    adjust scales to handle negative cases, fix for reversed bounds

--- lib/pick-fragment.glsl
+++ lib/pick-fragment.glsl
@@ -6,6 +6,16 @@ uniform float pickId;
 varying vec3 f_position;
 varying vec4 f_id;
 
+bool outOfRange(float a, float b, float p) {
+  if (p > max(a, b)) return true;
+  if (p < min(a, b)) return true;
+  return false;
+}
+
 void main() {
+  if (outOfRange(clipBounds[0].x, clipBounds[1].x, f_position.x)) discard;
+  if (outOfRange(clipBounds[0].y, clipBounds[1].y, f_position.y)) discard;
+  if (outOfRange(clipBounds[0].z, clipBounds[1].z, f_position.z)) discard;
+
   gl_FragColor = vec4(pickId, f_id.xyz);
 }
