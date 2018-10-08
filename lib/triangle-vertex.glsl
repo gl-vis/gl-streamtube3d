5f2cf5e866865d32cc6b9058247c627af27549ac
archmoj <mojtaba@plot.ly>
2018-10-08 18:37:12 -0400

    streamtubes translation

--- lib/triangle-vertex.glsl
+++ lib/triangle-vertex.glsl
@@ -30,7 +30,7 @@ void main() {
   vec4 tubePosition = model * vec4(position.xyz, 1.0) + vec4(XYZ, 0.0);
   normal = normalize(normal * inverse(mat3(model)));
 
-  gl_Position      = projection * (view * tubePosition);
+  gl_Position      = projection * view * tubePosition;
   f_color          = color;
   f_normal         = normal;
   f_data           = tubePosition.xyz;
